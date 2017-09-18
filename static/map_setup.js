0ea2ce5decd092c72f7aaf8839a6b7e6e8f56d52
Fidel Serrano <serranoycandela@gmail.com>
2017-09-18 16:36:12 -0500

    paint

--- static/map_setup.js
+++ static/map_setup.js
@@ -1,3 +1,10 @@
+function paint(ageb_ids){
+     vectorSource.clear();
+     estosFeatures = todos.filter(function (feature) {return ageb_ids.indexOf(feature.get('AGEB_ID')) >= 0;});
+     vectorSource.addFeatures(estosFeatures);
+
+}
+
 function get_features(url) {
     var data_layer = {};
 
@@ -56,15 +63,44 @@ var style = function(feature, resolution){
 	};
 
 var map
+
 var layer = new ol.layer.Vector();
 jsonSource_data_layer = new ol.source.Vector();
 jsonSource_data_layer.addFeatures(get_features("/static/bayesianPreEnch.json"));
-
+var todos = jsonSource_data_layer.getFeatures();
 layer = new ol.layer.Vector({
     source: jsonSource_data_layer,
     style: style,
 	opacity: 0.85
 });
+
+var style2 = new ol.style.Style({
+	    	  fill: new ol.style.Fill({
+	    	    color: 'rgba(70,130,180,0.7)'
+	    	  }),
+	    	  stroke: new ol.style.Stroke({
+	    	    color: '#319FD3',
+	    	    width: 1
+	    	  }),
+	    	  text: new ol.style.Text({
+	    	    font: '12px Calibri,sans-serif',
+	    	    fill: new ol.style.Fill({
+	    	      color: 'rgba(250,163,1,1)'
+	    	    }),
+	    	    stroke: new ol.style.Stroke({
+	    	      color: 'rgba(100,100,100,1)',
+	    	      width: 3
+	    	    })
+	    	  })
+	    	});
+       
+	var vectorSource = new ol.source.Vector({
+     		projection: 'EPSG:4326'
+     	});
+     	var miVector = new ol.layer.Vector({
+        	source: vectorSource,
+        	style: style2
+    	}); 
     
 var stamenLayer = new ol.layer.Tile({
 	source: new ol.source.Stamen({
@@ -76,13 +112,14 @@ var stamenLayer = new ol.layer.Tile({
 
 map = new ol.Map({
      		projection:"EPSG:4326",
-     		layers: [stamenLayer],
+    layers: [stamenLayer, layer, miVector],
      		target: 'map',
      		view: new ol.View({
 	       		center: ol.proj.fromLonLat([-99.15,19.36]),
 	       		zoom: 11
       		})
 });
-	
-map.addLayer(layer);
+
+
+
 	

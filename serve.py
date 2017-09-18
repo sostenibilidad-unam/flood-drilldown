0d904739dfbc43366a5dc7d93dbd0ac70ace28d2
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-09-18 16:24:24 -0500

    hardcoded table

--- serve.py
+++ serve.py
@@ -1,9 +1,30 @@
 from jinja2 import Environment, FileSystemLoader
+import json
+from pprint import pprint
+
 
 from flask import Flask, make_response, request, send_from_directory
 app = Flask(__name__)
 
 env = Environment(loader=FileSystemLoader('templates'))
+
+def load_agebs():
+    agebs = {}
+        
+    with open('static/bayesianPreEnch.json') as geo:
+        g = json.loads(geo.read())
+        for f in g['features']:
+            k = (f['properties']['FrecCateg'], f['properties']['PrecCateg'])
+            if k in agebs:
+                agebs[k].append(f['properties']['AGEB_ID'])
+            else:
+                agebs[k] = [f['properties']['AGEB_ID'], ]
+
+    return agebs
+
+
+
+agebs = load_agebs()
 
 
 @app.route('/table/')
@@ -19,3 +40,5 @@ def serve_static(path):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+
